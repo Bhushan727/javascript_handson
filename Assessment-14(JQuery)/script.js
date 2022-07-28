1362ecd1933ca87a680c6587bee9bd9e9a5794d8
shashi Bhushan <bhushans727@gmail.com>
2022-07-28 16:29:09 +0530

    added Assesment 14 JQuer

---
+++ Assessment-14(JQuery)/script.js
@@ -0,0 +1,17 @@
+
+// Ques 1
+// Create a button , on click of which you should be able to hide a paragraph text, create another button onclick of which this paragraph should be visible
+
+$('#hide').click(function(){
+    $('#hideNsheek').hide();
+})
+$('#show').click(function(){
+    $('#hideNsheek').show();
+})
+
+
+
+
+
+
+
